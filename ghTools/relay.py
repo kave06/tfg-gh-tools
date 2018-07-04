7c05065c2fa3605c6af8981e2955cf52abc852d1
kave06 <kave006@gmail.com>
2018-07-04 20:52:03 +0200

    update

--- ghTools/relay.py
+++ ghTools/relay.py
@@ -16,8 +16,8 @@ class Relay:
 
 
 if __name__ == '__main__':
-    relay = Relay(id=3, state='ON')
-    relay.set_state()
+    relay = Relay(id=4, state='ON')
+    relay.set_state('ON')
     sleep(2)
     relay.state = 'OFF'
-    relay.set_state()
+    relay.set_state('OFF')
