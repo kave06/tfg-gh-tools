4333da89c44f034c9423ab5c5c1bb640143a2b79
kave06 <kave006@gmail.com>
2018-07-14 18:38:43 +0200

    update

--- ghTools/logger.py
+++ ghTools/logger.py
@@ -1,30 +1,30 @@
 import logging
 
-class Logger():
-
-    def __init__(self, name:str, path : str='./log/'):
-        self.path = path
-        self.name = name
-
-        file_debug = path + name + '_debug.log'
-        file_hist = path +name + '.log'
-        formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(filename)-14s '
-                                      '%(funcName)-15s:%(lineno)-3s %(message)s')
-
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.DEBUG)
-
-        fh1 = logging.FileHandler(filename=file_debug, mode='w+')
-        fh2 = logging.FileHandler(filename=file_hist, mode='a+')
-
-        fh1.setLevel(logging.DEBUG)
-        fh2.setLevel(logging.ERROR)
-
-        fh1.setFormatter(formatter)
-        fh2.setFormatter(formatter)
-
-        logger.addHandler(fh1)
-        logger.addHandler(fh2)
+# class Logger():
+#
+#     def __init__(self, name:str, path : str='./log/'):
+#         self.path = path
+#         self.name = name
+#
+#         file_debug = path + name + '_debug.log'
+#         file_hist = path +name + '.log'
+#         formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(filename)-14s '
+#                                       '%(funcName)-15s:%(lineno)-3s %(message)s')
+#
+#         logger = logging.getLogger(__name__)
+#         logger.setLevel(logging.DEBUG)
+#
+#         fh1 = logging.FileHandler(filename=file_debug, mode='w+')
+#         fh2 = logging.FileHandler(filename=file_hist, mode='a+')
+#
+#         fh1.setLevel(logging.DEBUG)
+#         fh2.setLevel(logging.ERROR)
+#
+#         fh1.setFormatter(formatter)
+#         fh2.setFormatter(formatter)
+#
+#         logger.addHandler(fh1)
+#         logger.addHandler(fh2)
 
 
 
